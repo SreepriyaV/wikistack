36bc8eb1e930c58707f248ccadf2556fc4eb57fe
sreepriya <sreepriyav@live.com>
2017-09-22 17:24:07 -0400

    changes

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-const express = require ('express'); //node platform used to create servers
-const app = express();
-const morgan = require ('morgan'); //used to log a summary of all requests and responses 
-
-//get req line
-app.get('/', function(req, res){
-
-});
-
-//middleware
-app.use(function(req, res, next){
-	//do logging here
-	//call next or else your app will be a black hole
-	//receives requests and not properly responding
-})
-
-//listens to my server - good practice to have listen come up at the end
-app.listen(3000, function(){
-	console.log('listening on port 3000')
-});
-
-=======
 const express =require('express');
 const app=express();
 const morgan= require('morgan'); // log summary of client server comm 
@@ -41,4 +18,3 @@ app.use(function (req, res, next) {
 app.listen(3000, function () {
   console.log(' listening on port 3000!')
 })
->>>>>>> 8cbe995563aff95171a2ac8e3103853df3c93120
